1e3c7b1e2c1495db847d66b24df89cd4cc9407cd
JLM00-dot <jlmo@gmx.ch>
2024-10-21 15:51:18 +0200

    Basic Steuerung, Gravitation fehlt noch

--- Projektarbeit/Assets/Scripts/ArrowControler.cs
+++ Projektarbeit/Assets/Scripts/ArrowControler.cs
@@ -5,15 +5,22 @@ using UnityEngine;
 public class ArrowControler : MonoBehaviour
 {
     private float speed=10.0f;
-    private float launchspeed=100.0f;
+    public float launchspeed=10.0f;
     private float forwardInput;
     private float winkelInput;
     private Rigidbody playerRb;
+
+    private GameObject direction;
+    private bool inAir;
+
+    public float gravity=100.0f;
 
     // Start is called before the first frame update
     void Start()
     {
         playerRb=GetComponent<Rigidbody>();  
+        direction = GameObject.FindWithTag("Direction");
+        inAir=true;
     }
 
     // Update is called once per frame
@@ -23,19 +30,24 @@ public class ArrowControler : MonoBehaviour
         {
             launchspeed=launchspeed*1.1f;
         }
+        if (inAir)
+        {
         forwardInput = Input.GetAxis("Horizontal");
         transform.Rotate(Vector3.up*Time.deltaTime*speed*forwardInput);
         winkelInput = Input.GetAxis("Vertical");
         transform.Rotate(Vector3.forward*Time.deltaTime*speed*winkelInput);
         launch();
+        // playerRb.AddForce(Vector3.down*gravity,ForceMode.Force);  [Idee es runter zu drücken]
+        }
         
     }
     private void launch()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            playerRb.AddForce(transform.position*launchspeed,ForceMode.Impulse);
+            playerRb.AddForce(direction.transform.position*launchspeed,ForceMode.Acceleration);
             
+            inAir=false;
         }
     
     }
