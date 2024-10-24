9131191b5fe571befab51eeeb5edb2adc4e7e9ca
Greeggg <grzegorz.maj@stud.edubs.ch>
2024-10-24 11:18:55 +0200

    gui

--- Projektarbeit/Assets/Scripts/GuiManager.cs
+++ Projektarbeit/Assets/Scripts/GuiManager.cs
@@ -1,15 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GuiManager : MonoBehaviour
-public Button button;
 {
+    public Button startbutton;
+    public GameObject titleScreen;
+
+    void StartGame()
+    {
+        titleScreen.gameObject.SetActive(false);
+    }
+    
     // Start is called before the first frame update
     void Start()
     {
-        Button startbutton = button.GetComponent<Button>();
-        startbutton.OnClick.AddListener(StartGame);
+        startbutton = startbutton.GetComponent<Button>();
+        startbutton.onClick.AddListener(StartGame);
     }
 
     // Update is called once per frame
@@ -18,8 +26,4 @@ public Button button;
         
     }
     
-    void StartGame()
-    {
-        titleScreen.gameObject.SetActive(false);
-    }
 }
