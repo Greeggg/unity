db04d324f14d6be9a8f1fd711536cfd165fa0ae6
JLM00-dot <jlmo@gmx.ch>
2024-11-08 16:49:26 +0100

    Working target

--- Projektarbeit/Assets/Scripts/Target.cs
+++ Projektarbeit/Assets/Scripts/Target.cs
@@ -4,62 +4,70 @@ using UnityEngine;
 
 public class Target : MonoBehaviour
 {
-    public GameObject ring1;
-    public GameObject ring2;
-    public GameObject ring3;
-    public GameObject ring4;
-    public GameObject ring5;
-    public GameObject ring6;
-    public GameObject ring7;
-    public float score = 20 ;
-    // Start is called before the first frame update
+    public float score = 20;
+    private bool canhit;      
+    private float scoreCooldown = 0.1f; 
+
     void Start()
     {
-        
+        canhit=true;
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+
     }
-    private void OnTriggerEnter( Collider other)
+    private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject == ring1)
+        if (canhit)
         {
-            Debug.Log("ring1");
-            score-=1;
+            if (other.CompareTag("ring1"))
+            {
+                Debug.Log("ring1");
+                score -= 1;
+            }
+            else if (other.CompareTag("ring2"))
+            {
+                Debug.Log("ring2");
+                score -= 1;
+            }
+            else if (other.CompareTag("ring3"))
+            {
+                Debug.Log("ring3");
+                score -= 3;
+            }
+            else if (other.CompareTag("ring4"))
+            {
+                Debug.Log("ring4");
+                score -= 3;
+            }
+            else if (other.CompareTag("ring5"))
+            {
+                Debug.Log("ring5");
+                score -= 5;
+            }
+            else if (other.CompareTag("ring6"))
+            {
+                Debug.Log("ring6");
+                score -= 5;
+            }
+            else if (other.CompareTag("ring7"))
+            {
+                Debug.Log("ring7");
+                score -= 10;
+            }
+
+            Debug.Log( score);
+
+        
+            canhit = false;
+            StartCoroutine(ScoreCooldown());
         }
-        if (other.gameObject == ring2)
-        {
-            Debug.Log("ring2");
-            score-=1;
-        }
-        if (other.gameObject == ring3)
-        {
-            Debug.Log("ring3");
-            score-=3;
-        }
-        if (other.gameObject == ring4)
-        {
-            Debug.Log("ring4");
-            score-=3;
-        }
-        if (other.gameObject == ring5)
-        {
-            Debug.Log("ring5");
-            score-=5;
-        }
-        if (other.gameObject == ring6)
-        {
-            Debug.Log("ring6");
-            score-=5;
-        }
-        if (other.gameObject == ring7)
-        {
-            Debug.Log("ring7");
-            score-=10;
-        }
-        Debug.Log(score);
+    }
+
+    // Coroutine to handle the cooldown after the first score
+    private IEnumerator ScoreCooldown()
+    {
+        yield return new WaitForSeconds(scoreCooldown);
+        canhit = true; 
     }
 }
